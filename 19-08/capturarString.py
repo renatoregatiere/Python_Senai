a04daf41559eae0cf1cf2d48870c54b346efaf65
Renato Regatiere <renatoregatiere@gmail.com>
2023-08-19 14:09:30 -0300

    .

---
+++ 19-08/capturarString.py
@@ -0,0 +1,11 @@
+text = "parabens!"
+text2 = "parabens!  "
+text3 = "o rato roeu a roupa do rei de roma"
+
+ct1 = text[8]
+ct2 = text2[0]
+ct3 = text3[5]
+
+print(ct1)
+print(ct2)
+print(ct3)
---
+++ 19-08/concatenar.py
@@ -0,0 +1,8 @@
+oi = "ABCD"
+print(oi)
+oi2 = oi + "EFGHI"
+print(oi2)
+oi3 = oi + "x" * 10
+print(oi3)
+oi4 = oi * 2
+print(oi4)
