16c9a1dc4238d95198db4ca8f82453227853843a
Renato Regatiere <renatoregatiere@gmail.com>
2023-08-19 13:51:35 -0300

    1

---
+++ 19-08/comandoLen.py
@@ -0,0 +1,14 @@
+text = "parabens!"
+text2 = "parabens!  "
+text3 = "o rato roeu a roupa do rei de roma"
+
+lentexto = len(text)
+lentexto2 = len(text2)
+lentexto3 = len(text3)
+
+
+print(f"qtd carcateres de {text}: {lentexto}")
+print(f"qtd carcateres de {text2}: {lentexto2}")
+print(f"qtd carcateres de {text3}: {lentexto3}")
+
+print("----------------------------------------\n")
