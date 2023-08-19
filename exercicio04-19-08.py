c565227f1d10a7af37d946778143d94eb9a0cb54
Renato Regatiere <renatoregatiere@gmail.com>
2023-08-19 11:50:38 -0300

    Update exercicio04-19-08.py

--- exercicio04-19-08.py
+++ exercicio04-19-08.py
@@ -1,7 +1,7 @@
-n1 = int(input("Digite 1 ou 2: "));
+n1 = int(input("Digite 1 ou 2: "))
 if( n1 == 1):
-    print("Bom dia");
+    print("Bom dia")
 elif(n1 == 2):
-    print("Boa noite!");
+    print("Boa noite!")
 else:
     print("Número invalido!")
