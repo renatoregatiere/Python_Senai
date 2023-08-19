ecca52a795a75516f1425955ab9238821e37504b
Renato Regatiere <renatoregatiere@gmail.com>
2023-08-19 14:24:17 -0300

    1

---
+++ 19-08/composicaodeString.py
@@ -0,0 +1,6 @@
+a = 2
+b = 45.55555555
+print(f"variavel 'a' sem composição {a}")
+print(f"variavel 'a' com composição {a:06d}")
+print(f"variavel 'a' com composição {a:6d}")
+print(f"Douglas tem {45.00000000:5.2f} reais no bolso ")
--- 19-08/concatenar.py
+++ 19-08/concatenar.py
@@ -4,5 +4,7 @@ oi2 = oi + "EFGHI"
 print(oi2)
 oi3 = oi + "x" * 10
 print(oi3)
-oi4 = oi * 2
+oi4 = oi3 * 2
 print(oi4)
+oi5 = "y" * 50
+print(oi5)
