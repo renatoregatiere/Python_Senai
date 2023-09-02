98de113572de8bcb18b637887a582335f5049de9
Renato Regatiere <renatoregatiere@gmail.com>
2023-09-02 14:47:47 -0300

    Update exercicioRevisao4.py

--- 02-09/exercicioRevisao4.py
+++ 02-09/exercicioRevisao4.py
@@ -4,4 +4,4 @@
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('digite a segunda nota :'))
 
-print(f'A média de {nota1} e {nota2} é: {nota1/nota2}')
+print(f'A média de {nota1} e {nota2} é: {(nota1+nota2)/2}')
